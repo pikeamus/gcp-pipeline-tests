3b1545662f65d10da2e50af42b866db54e884c5f
pikeamus <32355704+pikeamus@users.noreply.github.com>
2019-03-03 16:13:02 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gcp_pipeline_tests',
+      version='0.1',
+      description='Simple pipeline for testing out gcp',
+      url='https://github.com/pikeamus/gcp-pipeline-tests',
+      author='pikeamus',
+      author_email='mikestevenroberts@gmail.com',
+      license='MIT',
+      packages=['gcp_pipeline_tests'],
+      zip_safe=False)
